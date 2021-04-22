6b54cece86c194be898294a9c44bc21edd924c4f
Erick2418 <erickjoellimon@outlook.com>
2021-04-21 22:48:13 -0500

    PropTypes

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,27 +1,21 @@
 import React, { Fragment } from 'react';
-// FUNCIONAL COMPONENT
-const PrimeraApp= () => {
-    //aqui los concatena pero los separa xd
-    //const saludo= [1,2,3,4];
-    /*const saludo={
-        nombre: 'Erick',
-        edad: 22
-    };*/
-    const saludo='Hola Mundo';
+import PropTypes from 'prop-types';
+
+const PrimeraApp= ({saludo}) => {
+
+    
     return ( 
         <>
             <h1>{saludo}</h1>
             {/*<pre> {JSON.stringify(saludo,null,3)} </pre>*/}
             <p>Mi primera Aplicacion</p>
         </>
-    ); 
-    //otra forma, tambien se coloca div si quieres el fragment por defalt sale si no colocas nada en la etiqueta
-    /* return ( 
-        <Fragment>
-            <h1>HOLA MUNDO</h1>
-            <p>Mi primera Aplicacion</p>
-        </Fragment>
-    );  */
+    );     
+}
+PrimeraApp.propTypes = {
+        
+    saludo: PropTypes.string.isRequired
     
 }
+
 export default PrimeraApp;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 //const saludo = <h1>HOLA MUNDO</h1>;
 const divRoot= document.querySelector('#root');
 //ReactDOM.render( saludo, divRoot);
-ReactDOM.render( <PrimeraApp />, divRoot);
+ReactDOM.render( <PrimeraApp saludo="Hola soy ERICK" />, divRoot);
 
 
 
