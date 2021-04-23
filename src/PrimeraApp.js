c3aabcc51eb78784aed2ddd1656aaf56b16daec8
Erick2418 <erickjoellimon@outlook.com>
2021-04-22 22:31:15 -0500

    default props

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,21 +1,23 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-const PrimeraApp= ({saludo}) => {
+const PrimeraApp= ({saludo, subtitulo='soy un subtitulo'}) => {
 
     
     return ( 
         <>
             <h1>{saludo}</h1>
+            <h1>{subtitulo}</h1>
+            
             {/*<pre> {JSON.stringify(saludo,null,3)} </pre>*/}
             <p>Mi primera Aplicacion</p>
         </>
     );     
 }
-PrimeraApp.propTypes = {
-        
+PrimeraApp.propTypes = {   
     saludo: PropTypes.string.isRequired
-    
 }
-
+PrimeraApp.defaultProps={
+    subtitulo: 'Soy un sub'
+}
 export default PrimeraApp;
