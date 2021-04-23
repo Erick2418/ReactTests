df0b54622c2ebe6a7e454263f3055efe67789d49
Erick2418 <erickjoellimon@outlook.com>
2021-04-23 11:37:58 -0500

    Hook-useState

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,11 +1,24 @@
-import React, { Fragment } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
+    /*UN Hook basicamente es un arreglo que tiene tanto valores como funciones*/ 
+    // se realiza una desestructuracion debido a que por default tiene un valor y una funcion 
+    const [counter,setCounter]= useState(0);// [,]
+    
+    
+
+    // handledadd
+    const handleAdd = (e)=>{
+        setCounter(counter+1);
+       // setCounter( (c)=>c+1);
+    }
+
     return (
         <>
         <h1>CounterApp</h1>
-        <h2>{value}</h2>
+        <h2>{counter}</h2>
+        <button onClick={ handleAdd } >+1</button>
         </>
     );
 }
